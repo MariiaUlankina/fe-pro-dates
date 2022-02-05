7e930f3bda859556a4957321e1413dbee14c62f7
unknown <ulankina.masha@gmail.com>
2022-02-05 21:33:06 +0200

    added function lastDay

--- js/dates.js
+++ js/dates.js
@@ -59,7 +59,10 @@ year – год из четырёх цифр, например, 2012.
 month – месяц от 0 до 11.
 К примеру, getLastDayOfMonth(2012, 1) = 29 (високосный год, февраль).
 */
-function getLastDayOfMonth(year, month) {}
+function getLastDayOfMonth(year, month) {
+  const day = new Date (year, month + 1, 0);
+  return day.getDate();
+}
 
 module.exports = {
   getDay,
